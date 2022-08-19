14bd4a26ec7ae44ea13ec1526d6cf8c232b4d352
armgp <arm.gp23@gmail.com>
2022-08-19 16:25:21 +0530

    dsaps q2 checked

--- DSAPS/Assg/Assg1_22-08-22/2022201024_q2.cpp
+++ DSAPS/Assg/Assg1_22-08-22/2022201024_q2.cpp
@@ -38,6 +38,7 @@ public:
 
     //member functions
     void resize();
+    void resize(int x, T d);
     void push_back(T x);
     void push_front(T x);
     void pop_back();
@@ -46,24 +47,9 @@ public:
     T back();
     bool empty();
     int size();
-    T operator[](int index);
+    T& operator[](int index);
+    void clear();
 };
-
-template <typename T> T Deque<T>::operator[](int index){
-        cout<<frontPtr<<", "<<backPtr<<" -->";
-        int i;
-        if(index>=qSize) {
-            cout<<"Index out of Bounds, returnng the first element";
-            i=frontPtr;
-        }
-        else{
-            i = (frontPtr-index)%capacity;
-            if(i<0){
-                i=capacity+1;
-            }
-        }
-        return arr[i];
-}
 
 template <typename T> void Deque<T>::resize(){
     int newCapacity = (3*capacity)/2;
@@ -81,6 +67,37 @@ template <typename T> void Deque<T>::resize(){
     }
 }
 
+//13. Check Again
+template <typename T> void Deque<T>::resize(int x, T d){
+    T* newArr = new T[x];
+    int i=backPtr;
+    int count=0;
+    for(int k=0; k<x; k++){
+        if(i==capacity) i=0;
+        if(count<qSize){
+            newArr[k]=arr[i++];
+            count++;
+        }else{
+            newArr[k]=d;
+        }
+    }
+
+    capacity=x;
+    arr=newArr;
+    frontPtr=qSize-1;
+    qSize=capacity;
+    backPtr=frontPtr+1<x?frontPtr+1:0;
+    cout<<"\n";
+}
+//12. OK
+template <typename T> void Deque<T>::clear(){
+    T* newArr = new T[10];
+    arr=newArr;
+    backPtr=-1;
+    frontPtr=-1;
+    capacity=10;
+    qSize=0;
+}
 //3. OK(check).
 template <typename T> void Deque<T>::push_back(T x){
     if(qSize==capacity) resize();
@@ -113,7 +130,6 @@ template <typename T> void Deque<T>::pop_back(){
         backPtr=(backPtr+1)%capacity;
         qSize--;
     }
-    cout<<frontPtr<<" "<<backPtr<<"\n";
 }
 //6. OK(check).
 template <typename T> void Deque<T>::pop_front(){
@@ -143,8 +159,20 @@ template <typename T> bool Deque<T>::empty(){
 template <typename T> int Deque<T>::size(){
     return qSize;
 }
-//11. []
-
+//11. [] OK.
+template <typename T> T& Deque<T>::operator[](int index){
+        int i;
+        if(index>=qSize) {
+            throw "Index out of bounds";
+        }
+        else{
+            i = (frontPtr-index)%capacity;
+            if(i<0){
+                i=capacity+i;
+            }
+        }
+        return arr[i];
+}
 
 signed main(){
     fast;
@@ -153,7 +181,7 @@ signed main(){
     // Iterate range [1, 100],
     // push even numbers at the back
     // and push odd numbers at the front
-    for (int i = 1; i < 17; i++)
+    for (int i = 1; i < 17; i++){
         if (i % 2 == 0){
             q.push_back(i);
             Q.push_back(i);
@@ -162,6 +190,7 @@ signed main(){
             q.push_front(i);
             Q.push_front(i);
         }
+    }
  
  
     // Print the current size
@@ -195,7 +224,22 @@ signed main(){
  
     // Print last element of the deque
     cout << "Rear element " << q.back() << endl;
-    cout<<q[2]<<"\n";
+    for(int i=0; i<q.size(); i++){
+        cout<<q[i]<<" ";
+    }
+    cout<<"\n";
+    q.resize(20, 99);
+    for(int i=0; i<q.size(); i++){
+        cout<<q[i]<<" ";
+    }
+    cout<<"\n";
+    cout<<q.back()<<"\n";
+    q.clear();
+    q.push_back(1012);
+    for(int i=0; i<q.size(); i++){
+        cout<<q[i]<<" ";
+    }
+    cout<<"\n"<<"---"<<"\n";
     cout<<"-------------------------------------------------------------------------"<<"\n";
 
  
@@ -230,7 +274,9 @@ signed main(){
  
     // Print last element of the deque
     cout << "Rear element " << Q.back() << endl;
-    cout<<Q[2];
+    for(int i=0; i<Q.size(); i++){
+        cout<<Q[i]<<" ";
+    }
     return 0;
 }
 
