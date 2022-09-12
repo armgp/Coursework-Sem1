8dadeecc049650f763f89b15a39249d905ed0c8d
armgp <arm.gp23@gmail.com>
2022-09-12 18:14:58 +0530

    q1a q1b completed

--- DSAPS/Assg/Assg2_19-09-22/2022201024_A2/2022201024_A2_Q1/2022201024_A2_Q1b.cpp
+++ DSAPS/Assg/Assg2_19-09-22/2022201024_A2/2022201024_A2_Q1/2022201024_A2_Q1b.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include<unordered_map>
-#include <set>
+#include <unordered_set>
 using namespace std;
 
 struct Node {
-   int data;
+   int key=0;
+   int value=0;
    int calls=0;
    struct Node* next=NULL;
    struct Node* prev=NULL;
@@ -12,9 +13,10 @@ struct Node {
 
 class LFUcache {
     int capacity;
-    unordered_map<int, int> cache;
-    Node* head=NULL;
-    Node* tail=NULL;
+    int minFrequency=1;
+    unordered_map<int, Node*> cache;
+    unordered_map<int, Node*> frequencyMapHead;
+    unordered_map<int, Node*> frequencyMapTail;
 
 public:
     LFUcache(int c);
@@ -26,132 +28,126 @@ LFUcache::LFUcache(int c){
     capacity = c;
 }
 
-void LFUcache::set(int key, int value){
+void LFUcache::set(int key, int value) {
     if(cache.find(key)!=cache.end()){
-        cache[key]=value;
-        Node* curr = head;
-        while(curr!=NULL){
-            if(curr->data == key){
-                break;
-            }
-            curr = curr->next;
-        }
-  
-        curr->calls++;
-        Node* displacedNode = curr;
-        
-        if(curr != head){
-            curr = curr->prev;
-            if(curr->calls > displacedNode->calls) return;
-            while(curr!=NULL && curr->calls<=displacedNode->calls){
-                curr = curr->prev;
-            }
-
-            displacedNode->prev->next = displacedNode->next;
-            if(displacedNode->next) displacedNode->next->prev = displacedNode->prev;
-            if(curr == NULL){
-                displacedNode->next = head;
-                displacedNode->prev = NULL;
-                head->prev = displacedNode;
-                head = displacedNode;
-            }else{
-                displacedNode->next = curr->next;
-                curr->next->prev=displacedNode;
-                curr->next = displacedNode;
-                displacedNode->prev = curr;
-            }
-        }
-
-    }else if(cache.size()<capacity){
-        cache[key]=value;
-        Node* newNode = new Node;
-        newNode->data = key;
-        newNode->calls++;
-        if(head==NULL){
-            head = newNode;
-            tail = newNode;
+        Node* node = cache[key];
+        node->value = value;
+        if(node->next==NULL && node->prev==NULL){
+            frequencyMapHead.erase(node->calls);
+            frequencyMapTail.erase(node->calls);
+        }else if(node->next==NULL){
+            frequencyMapTail[node->calls] = frequencyMapTail[node->calls]->prev;
+            node->prev->next = NULL;
+            node->prev = NULL;
+        }else if(node->prev==NULL){
+            frequencyMapHead[node->calls] = frequencyMapHead[node->calls]->next;
+            node->next->prev = NULL;
+            node->next = NULL;
         }else{
-            Node* curr = tail;
-            while(curr!=NULL && curr->calls==newNode->calls){
-                curr = curr->prev;
-            }
-
-            if(curr==NULL){
-                newNode->next = head;
-                head->prev = newNode;
-                head = newNode;
-            }else{
-                newNode->next = curr->next;
-                newNode->prev = curr;
-                curr->next = newNode;
-                if(newNode->next) newNode->next->prev = newNode;
-                else tail = newNode;
-            }
+            node->prev->next = node->next;
+            node->next->prev = node->prev;
+            node->next=NULL;
+            node->prev=NULL;
         }
-    }else if(cache.size() == capacity){
-        int rmKey = tail->data;
-        cache.erase(rmKey);
-        cache[key] = value;
-        tail->data = key;
-        tail->calls = 1;
-
-        Node* newNode = tail;
-        Node* curr = tail->prev;
-        while(curr!=NULL && curr->calls==newNode->calls){
-            curr = curr->prev;
+        node->calls++;
+        if(frequencyMapHead.find(node->calls)!=frequencyMapHead.end()){
+            Node* head = frequencyMapHead[node->calls];
+            node->next = head;
+            head->prev = node;
+            frequencyMapHead[node->calls]=node;
+        }else{
+            frequencyMapHead[node->calls]=node;
+            frequencyMapTail[node->calls]=node;
         }
+        if(frequencyMapHead.find(minFrequency)==frequencyMapHead.end()) minFrequency++;
+        }else if(cache.size()<capacity){
+        Node* newNode = new Node;
+        newNode->key = key;
+        newNode->value = value;
+        newNode->calls = 1;
 
-        if(curr==NULL){
+        if(frequencyMapHead.find(1)!=frequencyMapHead.end()){
+            Node* head = frequencyMapHead[1];
             newNode->next = head;
             head->prev = newNode;
-            head = newNode;
+            frequencyMapHead[1]=newNode;
         }else{
-            if(curr->next == newNode) return;
-            newNode->next = curr->next;
-            newNode->prev = curr;
-            curr->next = newNode;
-            if(newNode->next) newNode->next->prev = newNode;
-            else tail = newNode;
+            frequencyMapHead[1]=newNode;
+            frequencyMapTail[1]=newNode;
         }
+        minFrequency = 1;
+        cache[key] = newNode;
+    }else if(cache.size()==capacity){
+        if(capacity==0) return;
+        Node* rmNode = frequencyMapTail[minFrequency];
+        cache.erase(rmNode->key);
+        if(rmNode->next==NULL && rmNode->prev==NULL){
+            frequencyMapHead.erase(minFrequency);
+            frequencyMapTail.erase(minFrequency);
+        }else if(rmNode->next==NULL){
+            frequencyMapTail[rmNode->calls] = frequencyMapTail[rmNode->calls]->prev;
+            rmNode->prev->next = NULL;
+            rmNode->prev = NULL;
+        }else if(rmNode->prev==NULL){
+            frequencyMapHead[rmNode->calls] = frequencyMapHead[rmNode->calls]->next;
+            rmNode->next->prev = NULL;
+            rmNode->next = NULL;
+        }else{
+            rmNode->prev->next = rmNode->next;
+            rmNode->next->prev = rmNode->prev;
+            rmNode->next=NULL;
+            rmNode->prev=NULL;
+        }
+        Node* newNode = rmNode;
+        newNode->key = key;
+        newNode->value = value;
+        newNode->calls = 1;
+        if(frequencyMapHead.find(1)!=frequencyMapHead.end()){
+            Node* head = frequencyMapHead[1];
+            newNode->next = head;
+            head->prev = newNode;
+            frequencyMapHead[1]=newNode;
+        }else{
+            frequencyMapHead[1]=newNode;
+            frequencyMapTail[1]=newNode;
+        }
+        minFrequency = 1;
+        cache[key] = newNode;
     }
 }
 
-int LFUcache::get(int key){
+int LFUcache::get(int key) {
     if(cache.find(key)==cache.end()) return -1;
-
-    Node* curr = head;
-    while(curr!=NULL){
-        if(curr->data == key){
-            break;
-        }
-        curr = curr->next;
+    Node* node = cache[key];
+    if(node->next==NULL && node->prev==NULL){
+        frequencyMapHead.erase(node->calls);
+        frequencyMapTail.erase(node->calls);
+    }else if(node->next==NULL){
+        frequencyMapTail[node->calls] = frequencyMapTail[node->calls]->prev;
+        node->prev->next = NULL;
+        node->prev = NULL;
+    }else if(node->prev==NULL){
+        frequencyMapHead[node->calls] = frequencyMapHead[node->calls]->next;
+        node->next->prev = NULL;
+        node->next = NULL;
+    }else{
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+        node->next=NULL;
+        node->prev=NULL;
     }
-    curr->calls++;
-    Node* displacedNode = curr;
-        
-    if(curr != head){
-        curr = curr->prev;
-        if(curr->calls > displacedNode->calls) return cache[key];
-        while(curr!=NULL && curr->calls<=displacedNode->calls){
-            curr = curr->prev;
-        }
-        displacedNode->prev->next = displacedNode->next;
-        if(displacedNode->next) displacedNode->next->prev = displacedNode->prev;
-        if(curr == NULL){
-            displacedNode->next = head;
-            displacedNode->prev = NULL;
-            head->prev = displacedNode;
-            head = displacedNode;
-        }else{
-            displacedNode->next = curr->next;
-            curr->next->prev=displacedNode;
-            curr->next = displacedNode;
-            displacedNode->prev = curr;
-        }
+    node->calls++;
+    if(frequencyMapHead.find(node->calls)!=frequencyMapHead.end()){
+        Node* head = frequencyMapHead[node->calls];
+        node->next = head;
+        head->prev = node;
+        frequencyMapHead[node->calls]=node;
+    }else{
+        frequencyMapHead[node->calls]=node;
+        frequencyMapTail[node->calls]=node;
     }
-
-    return cache[key];
-
+    if(frequencyMapHead.find(minFrequency)==frequencyMapHead.end()) minFrequency++;
+    return node->value;
 }
 
 int main(){
