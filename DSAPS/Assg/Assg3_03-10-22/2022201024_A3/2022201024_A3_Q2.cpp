39b2c9a994e4bae2b490984585ee27d9cdc655fd
armgp <arm.gp23@gmail.com>
2022-09-26 18:45:37 +0530

    stable q2 not finished dsaps assg3

--- DSAPS/Assg/Assg3_03-10-22/2022201024_A3/2022201024_A3_Q2.cpp
+++ DSAPS/Assg/Assg3_03-10-22/2022201024_A3/2022201024_A3_Q2.cpp
@@ -2,14 +2,18 @@
 #include <stdio.h>
 #include  <vector>
 #include <algorithm>
+#include <iomanip>
+#include <time.h>
+// #include <chrono>
 using namespace std;
 
-#define LOC "../ex-sort-test/"
-#define TMPSIZE 10
+#define TMPSIZE 1000
 
+string LOC = "../ex-sort-test/";
+int nTempFiles=0;
 
 class minHeap{
-    vector<vector<long>> arr;
+    vector<vector<int>> arr;
 
 public:
     minHeap(){}
@@ -18,14 +22,14 @@ public:
         return arr.size()==0;
     }
     
-    void insert(long val, long index){
+    void insert(int val, int index){
         arr.push_back({val, index});
         int i = arr.size()-1;
         while(i!=0){
             int parent = (i-1)/2;
             if(arr[parent][0] > arr[i][0]){
-                long tempPV = arr[parent][0];
-                long tempPI = arr[parent][1];
+                int tempPV = arr[parent][0];
+                int tempPI = arr[parent][1];
                 arr[parent][0]=arr[i][0];
                 arr[parent][1]=arr[i][1];
                 arr[i][0]=tempPV;
@@ -48,8 +52,8 @@ public:
             minEleInd=rightChildInd;
         }
         if(minEleInd!=i){
-            long long tempV = arr[minEleInd][0];
-            long long tempI = arr[minEleInd][1];
+            int tempV = arr[minEleInd][0];
+            int tempI = arr[minEleInd][1];
             arr[minEleInd][0]=arr[i][0];
             arr[minEleInd][1]=arr[i][1];
             arr[i][0]=tempV;
@@ -58,13 +62,13 @@ public:
         }
     }
 
-    vector<long> poll(){
+    vector<int> poll(){
         int ind = arr.size();
         if(ind==0){
             return {};
         }
 
-        vector<long> res = {arr[0][0], arr[0][1]};
+        vector<int> res = {arr[0][0], arr[0][1]};
         arr[0][0]=arr[ind-1][0];
         arr[0][1]=arr[ind-1][1];
         arr.pop_back();
@@ -74,8 +78,6 @@ public:
     }
 
 };
-
-
 
 
 FILE* openFile(string fileName, string mode){
@@ -98,21 +100,21 @@ void sortFiles(vector<string> modularFiles, string op){
  
     minHeap h;
     for(int i=0; i<n; i++){
-        long val;
+        int val;
         FILE* file = openIpFiles[i];
         if(!feof(file)){
-            fscanf (file, "%ld", &val);  
+            fscanf (file, "%d", &val);  
             h.insert(val, i);
         }
     }
 
     while(!h.isEmpty()){
-        vector<long> smallestEleDet = h.poll();
-        long smallestEle = smallestEleDet[0];
-        long smallestEleInd = smallestEleDet[1];
-        fprintf(outfile, "%ld ", smallestEle);
-        long val;
-        if(fscanf(openIpFiles[smallestEleInd], "%ld", &val)==1){
+        vector<int> smallestEleDet = h.poll();
+        int smallestEle = smallestEleDet[0];
+        int smallestEleInd = smallestEleDet[1];
+        fprintf(outfile, "%d ", smallestEle);
+        int val;
+        if(fscanf(openIpFiles[smallestEleInd], "%d", &val)==1){
             h.insert(val, smallestEleInd);
         }
     }
@@ -125,14 +127,14 @@ void sortFiles(vector<string> modularFiles, string op){
 
 void externalSort(string ip, string op){
     FILE* inputFile = openFile(ip, "r");
-    vector<long> values(TMPSIZE);
     vector<string> modularFiles;
     int moduleNumber=1;
     bool stat = true;
     while(stat){
         int i=0;
+        vector<int> values(TMPSIZE, 0);
         for(i=0; i<TMPSIZE; i++){
-            if(fscanf(inputFile, "%ld", &values[i]) != 1){
+            if(fscanf(inputFile, "%d", &values[i]) != 1){
                 stat = false;
                 break;
             }
@@ -143,16 +145,38 @@ void externalSort(string ip, string op){
         sort(values.begin(), values.end());
         FILE* module = openFile(LOC+to_string(moduleNumber), "w");
         for(int j=0; j<i; j++){
-            fprintf(module, "%ld ", values[j]);
+            fprintf(module, "%d ", values[j]);
         }
         modularFiles.push_back(LOC+to_string(moduleNumber++));
         fclose(module);
     }
-
+    nTempFiles = modularFiles.size();
     sortFiles(modularFiles, op);
 }
 
-int main(){
-    externalSort("../ex-sort-test/input.txt", "../ex-sort-test/output_file.txt");
+int main(int argc, char *argv[]){
+    clock_t st, ed;
+    st = clock();
+    ios_base::sync_with_stdio(false);
+    // externalSort("../ex-sort-test/input.txt", "../ex-sort-test/output_file.txt");
+    string inf = argv[1];
+    string otf = argv[2];
+   
+    LOC="";
+    for(int i=otf.size()-1; i>=0; i--){
+        if(otf[i]=='/'){
+            LOC = otf.substr(0, i+1);
+            break;
+        }
+    }
+    externalSort(argv[1], argv[2]);
+
+    ed = clock();
+    double time = double(ed-st) / double(CLOCKS_PER_SEC);
+
+    cout<<"Number of integers in a temporary file: "<<TMPSIZE<<"\n";
+    cout<<"Number of temporary files created: "<<nTempFiles<<"\n";
+    cout <<"Total time taken by the program: "<<fixed<<setprecision(2)<<time<<"\n";
     return 0;
 }
+
