f62fc0c5e53074db31609ba07caf22267fd03635
armgp <arm.gp23@gmail.com>
2022-10-27 11:10:31 +0530

    ssd assg4 q1 done

---
+++ SSD/Assg/SSD_Assg4-2-11-22/2022201024/pattern.py
@@ -0,0 +1,25 @@
+def printTop():
+	print ('   ______  \n', ' /      \ \n', '/        \ ')
+	
+def printBottom():
+	print (' \        / \n', ' \______/ ')
+
+
+def printLine():
+	print(' +--------+ ')
+	
+def printStop():
+	print(' |  STOP  |')
+	
+def printAll():
+	printTop()
+	printLine()
+	printTop()
+	printBottom()
+	printBottom()
+	printLine()
+	printTop()
+	printStop()
+	printBottom()
+		
+printAll()
