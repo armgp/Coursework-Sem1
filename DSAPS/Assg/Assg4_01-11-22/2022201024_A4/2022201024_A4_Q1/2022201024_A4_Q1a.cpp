70e1281c6c4e15d0dbc9790b22702d914c2d5e5a
armgp <arm.gp23@gmail.com>
2022-11-01 19:34:31 +0530

    q1 ab done dsaps assg4

--- DSAPS/Assg/Assg4_01-11-22/2022201024_A4/2022201024_A4_Q1/2022201024_A4_Q1a.cpp
+++ DSAPS/Assg/Assg4_01-11-22/2022201024_A4/2022201024_A4_Q1/2022201024_A4_Q1a.cpp
@@ -1,25 +1,87 @@
 #include <iostream>
 #include <string.h>
-
+#include <limits.h>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+struct Suffix{
+    int index; 
+    vector<int> rank; 
+    Suffix(int i, int a, int b){
+        index = i;
+        rank.resize(2);
+        rank[0]=a;
+        rank[1]=b;
+    }
+};
+
+bool compare(struct Suffix s1, struct Suffix s2){
+    if(s1.rank[0]<s2.rank[0]) return true;
+    else if(s1.rank[0]>s2.rank[0]) return false;
+    else if(s1.rank[1]<s2.rank[1]) return true;
+    else if(s1.rank[1]>s2.rank[1]) return false;
+    return true;
+}
+
+vector<int> getSuffixArray(string s){
+
+    int n = s.size();
+    vector<struct Suffix> suffixes;
+
+    for(int i=0; i<n; i++){
+        int a = s[i]-'a';
+        int b = i+1<n ? s[i+1]-'a' : s[0]-'a';
+        struct Suffix suf(i, a, b);
+        suffixes.push_back(suf);
+    }
+
+    sort(suffixes.begin(), suffixes.end(), compare);
+    vector<int> indexes(n);
+
+    for(int j=4; j<n; j*=2){
+        int rank = 0;
+        vector<int> prevRank = {suffixes[0].rank[0], suffixes[0].rank[1]};
+        suffixes[0].rank[0] = rank;
+        indexes[suffixes[0].index] = 0;
+
+        for(int i=1; i<n; i++){
+            vector<int> currRank = {suffixes[i].rank[0], suffixes[i].rank[1]};
+            if(currRank == prevRank){
+                suffixes[i].rank[0] = rank;
+            }else suffixes[i].rank[0] = ++rank;
+            prevRank = currRank;
+            indexes[suffixes[i].index] = i;
+        }
+
+        for(int i=0; i<n; i++){
+            int nextIndex = suffixes[i].index+j/2;
+            suffixes[i].rank[1] = nextIndex<n ? suffixes[indexes[nextIndex]].rank[0] : s[0]-'a';
+        }
+
+        sort(suffixes.begin(), suffixes.end(), compare);
+    }
+
+    vector<int> result;
+    for(struct Suffix resSuf : suffixes){
+        result.push_back(resSuf.index);
+    }
+
+    return result;
+
+}
+
 int main(){
     string s;
     cin>>s;
 
-    vector<pair<string, int>> suffixArr;
+    vector<int> suffixArray = getSuffixArray(s);
+    int index = suffixArray[0];
+    string str= s+s;
     int n = s.size();
-    s+=s;
-    for(int i=0; i<n; i++){
-        suffixArr.push_back(make_pair(s.substr(i), i));
-    }
 
-    sort(suffixArr.begin(), suffixArr.end());
+    cout<<str.substr(index, n);
 
-    // for(auto st : suffixArr) cout<<st.first<<", "<<st.second<<"\n"
-    cout<<s.substr(suffixArr[0].second, n)<<"\n";
     return 0;
 }
